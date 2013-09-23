7fb13273d15db453ef027e7163f6a3435f923ae9
dmarij <dmarijancic@gmail.com>
2013-09-23 13:22:58 +0000

    tagging added

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,15 @@
 class Article < ActiveRecord::Base
   has_many :comments
+  has_many :taggings
+  has_many :tags, through: :taggings
   def num_of_comments
     self.comments.count
+  end
+  def tag_list
+    self.tags.join(", ")
+  end
+  def tag_list=(tags_string)
+    tag_names = tags_string.split(",").collect{|s| s.strip.downcase}.uniq
+    self.tags = tag_names.collect { |tag_name| Tag.find_or_create_by(name: tag_name) }
   end
 end
